64ffcd30521595b4ca9c55e1c4201a3c17edd01f
TRENDYOL\ahmetcan.guven <ahmetcanguven44@gmail.com>
2017-09-18 10:26:55 +0300

    output class details

--- script.user.js
+++ script.user.js
@@ -5,11 +5,13 @@
 // @description  try to take over the world!
 // @author       You
 // @match        **
+// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     var classList = [];
+    var outputList = [];
     var btn = document.createElement('button');
     btn.innerText = 'ClassList';
     btn.style.position = 'fixed';
@@ -17,23 +19,12 @@
     btn.style.left = '0px';
     btn.style.zIndex = '99999999999999';
     btn.onclick = generateClassList;
+    document.body.appendChild(btn);
     
-    init();
-    
-    function init(){
-        appendBlobSaver();
-        document.body.appendChild(btn);
-        setInterval(fetchClassList, 1000);
-    }
-
-    function appendBlobSaver(){
-        var script = document.createElement('script');
-        script.src = "https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.js";
-        document.getElementsByTagName('head')[0].appendChild(script);   
-    }
+    setInterval(fetchClassList, 1000);
         
     function generateClassList(){
-        download((location.hash+'-classList.txt').replace(/\//g,'').replace(/#/g,'').replace(/\!/g,''), classList.join('\r\n'));
+        download((location.hash+'-classList.txt').replace(/\//g,'').replace(/#/g,'').replace(/\!/g,''), outputList.join('\r\n'));
     }
     
     function fetchClassList(){
@@ -45,11 +36,28 @@
                     if(classList.indexOf(elementClassList[z]) == -1){
                         if(elementClassList[z].length > 0){
                             classList.push(elementClassList[z]);
+                            var data = getStyle(elementClassList[z]);
+                            if(data && data.length > 0){
+                                outputList.push(data);
+                            }
                         }
                     }
                 }
             }
         }
+    }
+    
+    function getStyle(className) {
+        for(var z = 0, lenz = document.styleSheets.length; z < lenz; z++){
+            var classes = document.styleSheets[z].rules || document.styleSheets[z].cssRules;
+            for (var x = 0, lenx = classes.length; x < lenx; x++) {
+                if (classes[x].selectorText && classes[x].selectorText.indexOf(className) > -1) {
+                    var classDetail = classes[x].cssText ? classes[x].cssText : classes[x].style.cssText;
+                    return classDetail;
+                }
+            }
+        }
+        return null;
     }
 
     function download(filename, text) {
